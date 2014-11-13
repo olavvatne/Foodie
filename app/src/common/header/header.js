e6a568aa52fb41c1ed54de5e7ab20e06fc49ceda
Olav Vatne <olavvatne@gmail.com>
2014-11-13 22:29:28 +0100

    Merge new javscript with directive. Maybe consider converting to more angular ways of doing the animation etc

--- app/src/common/header/header.js
+++ app/src/common/header/header.js
@@ -3,15 +3,41 @@ angular.module('fdCommon')
   return function(scope, element, attrs) {
       scope.minimized = false;
       angular.element($window).bind("scroll", function() {
-          var value = jQuery(window).scrollTop();
-          if(value > 310) {
-            scope.minimized = true;
+          var scrollTop = jQuery(window).scrollTop();
+          console.log("")
+          var navBP = 310; //breakpoint for banner/navigation
+          var navAniBP = 280; //breakpoint for the transition animation;
+          if(scrollTop > navAniBP) {
+            var value = (( scrollTop - navAniBP)/ (navBP - navAniBP));
+            var nSize = (100 - (30*value)).toFixed();
+           
+            document.getElementById("overlay").setAttribute("style", "opacity: "+value+";");
+            document.getElementById("pictureAni").setAttribute("style", "width: "+nSize+"px; height: "+nSize+"px;");
           }
-          else {
-            scope.minimized = false;
+          else { //Reset to original
+            document.getElementById("overlay").setAttribute("style", "opacity:0;");
+            document.getElementById("pictureAni").setAttribute("style", "width: 100px; height:100px;");
           }
-          scope.$apply();
-      });
+          if ((scrollTop > navBP)) {
+              if(!made) {
+                $('header').addClass("fix-user");
+                var div = "<div class='top__user'><div class='top__user--picture'> <img class='picture' src='http://www.dailynk.com/efile/201204/DNKF00009101_2.jpg'/></div></div>";
+                  $("header.fix-user").append(div);
+                  made = true;
+                }
+            }
+            else {
+              var node = $('header.fix-user');
+              while (node.firstChild) {
+                node.removeChild(node.firstChild);
+              }
+              node.empty();
+              $('header').removeClass("fix-user");
+              $("navBanner__contents").removeClass("startTrans");
+              made = false;
+            }
+            scope.$apply();
+        });
   };
 })
 .directive("navbar", function () {
