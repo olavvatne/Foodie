843b4f0043343917b60331be9747a65122a916fb
Olav Vatne <olavvatne@gmail.com>
2014-11-17 17:46:23 +0100

    Added code that disappeared in the merge

--- app/src/recipe/recipe.js
+++ app/src/recipe/recipe.js
@@ -83,7 +83,7 @@ function (baseService) {
         '<button class="btn--negative" ng-click="removeStep($index)"><b>X</b></button>' +
         '<span><textarea type="text" ng-model="model[$index]" ></textarea></span>' +
       '</li>'+
-      '<button class="btn--neutral" ng-click="addStep()">Add step</button>' +
+      '<button type="button" class="btn--neutral" ng-click="addStep()">Add step</button>' +
     '</ul>',
     scope: {
     model: '=ngModel'
@@ -127,7 +127,7 @@ function (baseService) {
         '<input  class="ingredient-name" style="width: 45%" placeholder="name"type="text" ng-model="model[$index].name" />' +
         '<button class="btn--negative" ng-click="removeStep($index)" ng-show="$index >0"><b>X</b></button>' +
       '</li></ul>'+
-      '<button class="btn--neutral" ng-click="addStep()" style="float:right">Add ingredients</button>',
+      '<button type="button" class="btn--neutral" ng-click="addStep()" style="float:right">Add ingredients</button>',
     controller: ['$scope', function($scope) {
       $scope.addStep= function() {
         $scope.model.push({quantity: 0, unit: '', name: ''});
